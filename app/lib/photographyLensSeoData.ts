20918b4364d4dd4c8702be8f6590e020049f2c32
John Nguyen <jnguyen92780@gmail.com>
2026-01-02 17:28:37 -0800

    componentized lens pages

--- app/lib/photographyLensSeoData.ts
+++ app/lib/photographyLensSeoData.ts
@@ -1,11 +1,6 @@
-import {photographyLensSeoData as seoDataNikkor35105Ais} from '../routes/photography.film-and-gear.nikkor-35mm-105mm-f35-f45-ais';
-import {photographyLensSeoData as seoDataNikkor105Ais} from '../routes/photography.film-and-gear.nikkor-105mm-f25-ais';
-import {photographyLensSeoData as seoDataNikkor28Ais} from '../routes/photography.film-and-gear.nikkor-28mm-f28-ais';
-import {photographyLensSeoData as seoDataNikkor50Ais} from '../routes/photography.film-and-gear.nikkor-50mm-f18-ais';
-import {photographyLensSeoData as seoDataNikkor80200Ais} from '../routes/photography.film-and-gear.nikkor-80mm-200mm-f4-ais';
 import {photographyLensSeoData as seoDataSigma105} from '../routes/photography.film-and-gear.sigma-105mm-f28-os-hsm-macro';
 
-export const pentax25mmF35HdHf = {
+export const pentax25mmF35HdHf: PageSeoData = {
   title: 'Pentax 25mm f/3.5 HD HF Lens',
   description: 'This is about the Pentax 25mm f/3.5 HD lens.',
   url: 'https://www.johnnguyen.codes/photography/film-and-gear/pentax-25mm-f35-hd-hf',
@@ -24,12 +19,126 @@ export const pentax25mmF35HdHf = {
   ],
 };
 
+export const nikkor35mm105mmF3545Ais: PageSeoData = {
+  title: 'Nikkor 35mm - 105mm f/3.5 - f/4.5 AI-S',
+  description: 'This is about the Nikkor 35mm - 105mm f/3.5 - 4.5 AI-S lens.',
+  url: 'https://www.johnnguyen.codes/photography/film-and-gear/nikkor-35mm-105mm-f35-f45-ais',
+  relativeUrlPath: '/photography/film-and-gear/nikkor-35mm-105mm-f35-f45-ais',
+  metaobjectType: 'lens',
+  pageType: 'photography',
+  updatedAt: '2025-11-26T12:53:28-08:00',
+  publishedAt: '2020-05-05T03:20:10-07:00',
+  media: [
+    {
+      url: 'https://cdn.shopify.com/s/files/1/0934/9293/6987/files/750x600.jpg?v=1763844438',
+      width: 750,
+      height: 600,
+      altText: 'This is the photagraphy home page featured image',
+    },
+  ],
+};
+
+export const nikkor105mmF25Ais: PageSeoData = {
+  title: 'Nikkor 105mm f/2.5 AI-S',
+  description: 'This is about the Nikkor 105mm f/2.5 AI-S lens.',
+  url: 'https://www.johnnguyen.codes/photography/film-and-gear/nikkor-105mm-f25-ais',
+  relativeUrlPath: '/photography/film-and-gear/nikkor-105mm-f25-ais',
+  metaobjectType: 'lens',
+  pageType: 'photography',
+  updatedAt: '2025-11-26T12:53:28-08:00',
+  publishedAt: '2020-05-05T03:20:10-07:00',
+  media: [
+    {
+      url: 'https://cdn.shopify.com/s/files/1/0934/9293/6987/files/750x600.jpg?v=1763844438',
+      width: 750,
+      height: 600,
+      altText: 'This is the photagraphy home page featured image',
+    },
+  ],
+};
+
+export const nikkor28mmF28Ais = {
+  title: 'Nikkor 28mm f/2.8 AI-S Lens',
+  description: 'This is about the Nikkor 28mm f/2.8 AI-S lens.',
+  url: 'https://www.johnnguyen.codes/photography/film-and-gear/nikkor-28mm-f28-ais',
+  relativeUrlPath: '/photography/film-and-gear/nikkor-28mm-f28-ais',
+  metaObject: 'lens',
+  pageType: 'photography',
+  updatedAt: '2025-11-26T12:53:28-08:00',
+  publishedAt: '2020-05-05T03:20:10-07:00',
+  media: [
+    {
+      url: 'https://cdn.shopify.com/s/files/1/0934/9293/6987/files/750x600.jpg?v=1763844438',
+      width: 750,
+      height: 600,
+      altText: 'This is the photagraphy home page featured image',
+    },
+  ],
+};
+
+export const nikkor50mmF18Ais = {
+  title: 'Nikkor 50mm f1.8 AI-S Pancake Lens',
+  description: 'This is about the Nikkor 50mm f/1.8 AI-S lens.',
+  url: 'https://www.johnnguyen.codes/photography/film-and-gear/nikkor-105mm-f25-ais',
+  relativeUrlPath: '/photography/film-and-gear/nikkor-50mm-f18-ais',
+  metaObject: 'lens',
+  pageType: 'photography',
+  updatedAt: '2025-11-26T12:53:28-08:00',
+  publishedAt: '2020-05-05T03:20:10-07:00',
+  media: [
+    {
+      url: 'https://cdn.shopify.com/s/files/1/0934/9293/6987/files/750x600.jpg?v=1763844438',
+      width: 750,
+      height: 600,
+      altText: 'This is the photagraphy home page featured image',
+    },
+  ],
+};
+
+export const nikkor80mm200mmF4Ais = {
+  title: 'Nikkor 80mm - 200mm f/4 AI-S',
+  description: 'This is about the Nikkor 80mm - 200mm f/4 AI-S lens.',
+  url: 'https://www.johnnguyen.codes/photography/film-and-gear/nikkor-80mm-205mm-f4-ais',
+  relativeUrlPath: '/photography/film-and-gear/nikkor-80mm-105mm-f4-ais',
+  metaobjectType: 'lens',
+  pageType: 'photography',
+  updatedAt: '2025-11-26T12:53:28-08:00',
+  publishedAt: '2020-05-05T03:20:10-07:00',
+  media: [
+    {
+      url: 'https://cdn.shopify.com/s/files/1/0934/9293/6987/files/750x600.jpg?v=1763844438',
+      width: 750,
+      height: 600,
+      altText: 'This is the photagraphy home page featured image',
+    },
+  ],
+};
+
+export const sigma105mmF28OsHsmMacro = {
+  title: 'Sigma 105mm f/2.8 EX DG OS HSM Lens',
+  description: 'This is about the Sigma 105mm f/2.8 EX DG OS HSM lens.',
+  url: 'https://www.johnnguyen.codes/photography/film-and-gear/sigma-105mm-f28-os-hsm-macro',
+  relativeUrlPath: '/photography/film-and-gear/sigma-105mm-f28-os-hsm-macro',
+  metaobjectType: 'lens',
+  pageType: 'photography',
+  updatedAt: '2025-11-26T12:53:28-08:00',
+  publishedAt: '2020-05-05T03:20:10-07:00',
+  media: [
+    {
+      url: 'https://cdn.shopify.com/s/files/1/0934/9293/6987/files/750x600.jpg?v=1763844438',
+      width: 750,
+      height: 600,
+      altText: 'This is the photagraphy home page featured image',
+    },
+  ],
+};
+
 export const photographyLensSeoData = [
-  seoDataNikkor35105Ais,
-  seoDataNikkor105Ais,
-  seoDataNikkor28Ais,
-  seoDataNikkor50Ais,
-  seoDataNikkor80200Ais,
+  nikkor35mm105mmF3545Ais,
+  nikkor105mmF25Ais,
+  nikkor28mmF28Ais,
+  nikkor50mmF18Ais,
+  nikkor80mm200mmF4Ais,
   pentax25mmF35HdHf,
-  seoDataSigma105,
+  sigma105mmF28OsHsmMacro,
 ];
