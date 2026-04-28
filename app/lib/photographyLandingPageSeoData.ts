c42513ab1e1411f322288dd673acd361c82a58aa
John Nguyen <jnguyen92780@gmail.com>
2026-04-28 08:04:39 -0700

    added seo data back

--- app/lib/photographyLandingPageSeoData.ts
+++ app/lib/photographyLandingPageSeoData.ts
@@ -17,24 +17,24 @@ export const photography: PageSeoData = {
   ],
 };
 
-// export const photographyFilmAndGear: PageSeoData = {
-//   status: 'active',
-//   title: 'Photography Film and Gear',
-//   description: 'My home page for my photography film and gear',
-//   url: 'https://www.johnnguyen.codes/photography/film-and-gear',
-//   relativeUrlPath: '/photography/film-and-gear',
-//   pageType: 'photography',
-//   updatedAt: '2025-11-26T12:53:28-08:00',
-//   publishedAt: '2020-05-05T03:20:10-07:00',
-//   media: [
-//     {
-//       url: 'https://cdn.shopify.com/s/files/1/0934/9293/6987/files/photography--2025-10-05--007--half-frame--pentax-17--pentax-25mm-f35-hd-hf--fujifilm--400--unknown--unknown.webp',
-//       width: 2048,
-//       height: 1365,
-//       altText: 'This is the photagraphy film and gear page featured image',
-//     },
-//   ],
-// };
+export const photographyFilmAndGear: PageSeoData = {
+  status: 'active',
+  title: 'Photography Film and Gear',
+  description: 'My home page for my photography film and gear',
+  url: 'https://www.johnnguyen.codes/photography/film-and-gear',
+  relativeUrlPath: '/photography/film-and-gear',
+  pageType: 'photography',
+  updatedAt: '2025-11-26T12:53:28-08:00',
+  publishedAt: '2020-05-05T03:20:10-07:00',
+  media: [
+    {
+      url: 'https://cdn.shopify.com/s/files/1/0934/9293/6987/files/photography--2025-10-05--007--half-frame--pentax-17--pentax-25mm-f35-hd-hf--fujifilm--400--unknown--unknown.webp',
+      width: 2048,
+      height: 1365,
+      altText: 'This is the photagraphy film and gear page featured image',
+    },
+  ],
+};
 
 export const photographyPhotos: PageSeoData = {
   status: 'active',
@@ -77,7 +77,7 @@ export const photographyPhotos: PageSeoData = {
 
 export const photographyLandingPageSeoData = [
   photography,
-  // photographyFilmAndGear,
+  photographyFilmAndGear,
   photographyPhotos,
   // photographyJournal,
 ];
